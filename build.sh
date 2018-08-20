d5c82e10cb36a351589d9f49754de5f983047d24
Naoki Matsumoto <m.naoki9911@gmail.com>
2018-08-20 10:35:54 +0900

    fixed build.sh to build ovmf

--- build.sh
+++ build.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+if [ -e image/EFI/BOOT ]; then
+  mkdir -p image/EFI/BOOT
+fi
+
 cd edk2
+git checkout xv6_uefiloader
 source edksetup.sh
-build -a X64
+if [ -e edk2/Build/OvmfX64/RELEASE_GCC5/FV ]; then
+        make -C edk2/BaseTools/Source/C
+        build -p OvmfPkg/OvmfPkgX64.dsc
+fi
+build
 
 cd ../xv6_public
+git checkout xv6
 make kernelmemfs
 cp kernelmemfs ../image/kernel
 
